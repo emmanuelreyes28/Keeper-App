ba510e006858984bd07a1652c220e80c63248d66
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-10-20 23:32:47 -0700

    Initial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentDate = new Date();
+
+
+function Footer(){
+    return(
+        <Footer>Copyright {currentDate}</Footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Header(){
+    return(
+        <h1>Keeper</h1>
+    );
+}
+
+export default Header;
