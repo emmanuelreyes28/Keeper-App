693ead09fce3035dbbdc98dc8164a019684a0065
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2022-10-29 17:53:58 -0700

    Mapping and ES6 functions

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {createRoot} from "react-dom/client";
+import App from "./components/App";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<App />, document.getElementById("root"));
+
+//Challenge. Render all the notes inside notes.js as a seperate Note
+//component.
